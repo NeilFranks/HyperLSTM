e94fff9576fafaf37e5896b3206e421c7b609edc
LSaldyt <lucassaldyt@gmail.com>
2022-04-24 21:11:43 -0700

    Rename

--- datasets/hockey.py
+++ datasets/hockey.py
@@ -1,23 +1,36 @@
 import random
 
 import pandas as pd
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-class HockeyDataset(Dataset):
-    def __init__(self, file_name):
-        self.X_COLUMNS = [
-            "Year", "Month", "Day",
-            "Home_ID", "Home_wins_last10", "Home_wins_VERSUS_last2",
-            "Home_goals_lastGame", "Home_assists_lastGame",
-            "Home_GA_startingGoalie", "Home_SA_startingGoalie",
-            "Home_GA_allGoalies", "Home_SA_allGoalies",
-            "Away_ID", "Away_wins_last10", "Away_wins_VERSUS_last2",
-            "Away_goals_lastGame", "Away_assists_lastGame",
-            "Away_GA_startingGoalie", "Away_SA_startingGoalie",
-            "Away_GA_allGoalies", "Away_SA_allGoalies"
-        ]
+def pad_and_add_channel(t, length, axis=0, channel_axis=1, index=None):
+    assert len(t.shape) > 1, 'Cannot pad a 1D tensor'
+    assert axis != channel_axis
+    # First, edit the tensor's shape to reflect padding
+    edit_shape = list(t.shape)
+    original = edit_shape[axis]
+    edit_shape[axis] = length
+    # Then, add a channel to the OTHER axis for masking
+    if channel_axis is not None:
+        edit_shape[channel_axis] += 1
+    # Create the background tensor from edited shape
+    background = torch.zeros(tuple(edit_shape))
+    # Calculate where to place our tensor
+    if index is None:
+        index = (0,) * len(t.shape) # Starting points for embed
+    slices = [np.s_[i:i+j] for i, j in zip(index, t.shape)]
+    background[slices] = t
+    if channel_axis is not None:
+        background[:original, -1] = torch.ones(original)
+    return background
 
+class MinimalHockeyDataset(Dataset):
+    def __init__(self, file_name, features, pad_length=20):
+        self.pad_length=pad_length
+
+        self.X_COLUMNS = features
         self.Y_COLUMN = "Home_Won"
 
         self.data = pd.read_csv(file_name)
@@ -113,4 +126,12 @@ class HockeyDataset(Dataset):
                     (y_sequence, y)
                 )
 
+        # Important! Pad sequences to a common length (20 in our case)
+        x_sequence = pad_and_add_channel(
+             x_sequence, self.pad_length, axis=0, channel_axis=1
+             )
+
+        y_sequence = pad_and_add_channel(
+            y_sequence, self.pad_length, axis=0, channel_axis=None # Don't add channel here
+            )
         return x_sequence, y_sequence
