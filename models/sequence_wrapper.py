5 game sequences, concatenated home and away. Every year in the dataset. fixed huge data leak. going for gold.

models/sequence_wrapper.py
@@ -111,7 +111,7 @@ class SequenceWrapper(pl.LightningModule):
         # Use this if we don't want SAM
         optimizer = torch.optim.Adam(
             self.parameters(),
-            lr=0.0002
+            lr=0.00018
         )
 
         # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(